c65eb8f91204d2fc63904a7bf3cf1bcef81a2345
Neil Garcia <neilgrande2002@gmail.com>
2025-07-19 16:57:24 +0800

    Imported and used react-toastify in adding and deleting of jobs

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/navbar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const MainLayout = () => {
   return (
@@ -9,6 +11,7 @@ const MainLayout = () => {
 
       <Navbar />
       <Outlet />
+      <ToastContainer />
       
     </div>
 
