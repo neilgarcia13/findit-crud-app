931323d9d4af97ebb92340f475b5762a498e3be3
Neil Garcia <neilgrande2002@gmail.com>
2025-07-21 15:06:15 +0800

    Wrote scripts for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/findit-crud-app",
   server: {
     port: 3000,
     proxy: {
